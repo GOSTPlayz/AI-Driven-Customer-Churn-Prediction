84ceea6cd746482ff7fe2637326f8a22a473b3ee
Sathvik_Gunda <72628203+GOSTPlayz@users.noreply.github.com>
2025-03-04 13:20:30 -0600

    Update dockertest.py
    Documentation Changes

--- dockertest.py
+++ dockertest.py
@@ -2,7 +2,6 @@ import requests
 
 url = "https://deu9qakbo8.execute-api.us-east-1.amazonaws.com/prod/predict"
 
-# ✅ Ensure the payload is correctly formatted
 data = {"features": [0, 1, 80.5, 500.0]}  # Example input
 headers = {"Content-Type": "application/json"}
 
